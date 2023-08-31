180f59c83dc50e2ccb8755c966fa521b0754c9f9
saggita1 <ryanpimentel52@gmail.com>
2023-08-31 13:09:16 -0400

    questão 2 letra B finalizada

--- atvb.asm
+++ atvb.asm
@@ -1,3 +1,6 @@
+.data
+	saida: .asciiz  "J = "
+
 .text
 	# j = $t0 e i = $t1
 	
@@ -20,6 +23,11 @@
 	addi $t1, $t1, 12 # adiciona 12 ao $t1(i)
 	add $t0, $t0, $t1 # adicionando $t1(i) ao $t0(j)
 	
+	# print saida
+	li $v0, 4
+	la $a0, saida
+	syscall
+	
 	# printando o novo J
 	li $v0, 1
 	move $a0, $t0
@@ -32,6 +40,11 @@
 true:
 	add $t0, $t1, $t1 # "multiplicando" $t1 por 2
 	addi $t0, $t0, 8 # adicionando 8 após multiplicar
+	
+	# print saida
+	li $v0, 4
+	la $a0, saida
+	syscall
 	
 	# printando novo $t0(j)
 	li $v0, 1
